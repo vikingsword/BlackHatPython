3c6d86cd1950b4cc077874470480d8bf543ed219
vikingar <423141759@qq.com>
2023-06-01 22:50:36 +0800

    netcat

---
+++ ch2/netcat.py
@@ -0,0 +1,10 @@
+import shlex
+import subprocess
+
+
+def execute(cmd):
+    cmd = cmd.strip()
+    if not cmd:
+        return
+    output = subprocess.check_output(shlex.split(cmd), stderr=subprocess.STDOUT)
+    return output.decode()
